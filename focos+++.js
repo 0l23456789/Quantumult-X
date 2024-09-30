db96adaf714bd970ba08cd0671063f38f212cd29
0l23456789 <uummnnuummnn@gmail.com>
2024-09-30 11:59:19 +0800

    focos+++.js

--- focos+++.js
+++ focos+++.js
@@ -4,11 +4,11 @@
 > 下载地址：http://t.cn/AilyJ3mp
 > 解锁说明：解锁高级会员权限
 > 更新时间：2023-05-05
-
+          
           
 [rewrite_local]
   
-# > focos|focoslive☆解锁会员权限 2023-05-05
+# > focos|focoslive☆解锁会员权限（2023-05-05）
 ^https?:\/\/.*oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify\/apple) url script-response-body https://raw.githubusercontent.com/0l23456789/Quantumult-X/main/focos+++.js
 
 
@@ -18,3 +18,24 @@ hostname = *.oracle.bendingspoonsapps.com
 
 
 ***********************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var obj=JSON.parse($response.body);-1!=$request.url.indexOf("focoslive")?obj.me.active_subscriptions_ids=["com.focoslive.1y_t100_adj"]:obj.me.active_subscriptions_ids=["com.focos.1y_t80"],$done({body:JSON.stringify(obj)});
