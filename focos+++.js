6b64c9a4efde70b467938924ea714024ef2c92f5
0l23456789 <uummnnuummnn@gmail.com>
2024-09-30 11:52:10 +0800

    focos+++.js

---
+++ focos+++.js
@@ -0,0 +1,20 @@
+/***********************************
+
+> 应用名称：focos+focoslive
+> 下载地址：http://t.cn/AilyJ3mp
+> 解锁说明：解锁高级会员权限
+> 更新时间：2023-05-05
+
+          
+[rewrite_local]
+  
+# > focos|focoslive☆解锁会员权限 2023-05-05
+^https?:\/\/.*oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify\/apple) url script-response-body https://raw.githubusercontent.com/0l23456789/Quantumult-X/main/focos+++.js
+
+
+[mitm]
+ 
+hostname = *.oracle.bendingspoonsapps.com
+
+
+***********************************/
